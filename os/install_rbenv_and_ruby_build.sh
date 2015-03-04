6bb9d1283e1f716dd5ace969993f52d5d81e433e
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2015-03-03 21:02:46 -0300

    Adds install_rbenv_and_ruby_build.sh

---
+++ os/install_rbenv_and_ruby_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")" && source "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    declare -r OS="$(get_os)"
+
+    if [ "$OS" == "osx" ]; then
+        execute "brew install rbenv ruby-build"
+    fi
+
+}
+
+main
