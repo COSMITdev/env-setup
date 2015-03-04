5118f69190552367bd3a03d10817eb810384c613
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2015-03-03 21:31:34 -0300

    Update install_rbenv_and_ruby_build.sh

--- os/install_rbenv_and_ruby_build.sh
+++ os/install_rbenv_and_ruby_build.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")" && source "utils.sh"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
-    execute "brew install rbenv ruby-build"
+    brew install rbenv ruby-build
 }
 
 main
